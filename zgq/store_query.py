f3bdee309770c19dc29a09b534750d2c42ad17b0
zhu61577542 <zhu61577542@163.com>
2018-01-19 11:20:50 +0800

    http status code

--- zgq/store_query.py
+++ zgq/store_query.py
@@ -1,7 +1,16 @@
 import csv
 import re
+'''
+domain tag 匹配程度
+完全匹配时值为 High
+包含对方相应单词 并且单词完全匹配时值为 Mid
+不匹配时值为 Low
 
-del_str = ['bon','d\'achat','achat','de','reduction','reduc','réduction','code','avantage','bonus','privilege','privilège','operation','opération','offre','promo','la','le','les','made'
+'''
+
+
+
+del_str = ['d\'achat','achat','de','reduction','réduction','code','avantage','bonus','privilege','privilège','operation','opération','offre','promo','les','made'
     ,'maison','du','remise']
 
 file = 'FR_Store_Query.csv'
@@ -18,6 +27,9 @@ def DomainToWords(reader):
 
 
 def RemoveCouponWords(query):
+    query = query.replace('\'','')
+    query = query.replace('&','')
+    query = query.replace('é','e')
     words = query.split()
     good_words = []
     for word in words:
@@ -25,24 +37,54 @@ def RemoveCouponWords(query):
             good_words.append(word)
     return good_words
 
+def diff(domain_words,query_words):
+    str_domain_words = ''.join(domain_words)
+    str_query_words = ''.join(query_words)
 
-
-output = open("query_store_check.tsv", "w")
-
-content= read_file()
-
-for i in content:
-    domain = i[0]
-    tag = i[3]
-    domain_words = DomainToWords(domain)
-    query_words = RemoveCouponWords(tag)
-    checked_result = 'Unclear'
     if domain_words == query_words:
-        checked_result = 'High'
-    elif ''.join(domain_words) in query_words:
-        checked_result = 'Mid'
-    else:
-        checked_result = 'Low'
-    i[4] = checked_result
-    output.write("%s\n" % ("\t".join(i)))
+        return 'High'
+
+
+    elif str_domain_words == str_query_words:
+        return 'High'
+
+
+
+    for i in domain_words:
+        if i == str_query_words:
+            return "High"
+        elif i in str_query_words:
+            return 'Mid'
+
+    for a in query_words:
+        if a == str_domain_words:
+            return 'High'
+        elif a in str_domain_words:
+            return "Mid"
+
+
+
+
+if __name__ == '__main__':
+    output = open("query_store_check.tsv", "w")
+    content= read_file()
+
+    for i in content:
+        domain = i[0]
+        tag = i[3]
+        #if domain != "showroomprive.com":
+        #    continue
+
+        domain_words = DomainToWords(domain)
+        query_words = RemoveCouponWords(tag)
+        #checked_result = 'Unclear'
+
+        checked_result = diff(domain_words,query_words)
+        if not checked_result:
+            checked_result = 'Low'
+
+
+        #print("%s\n" % ("\t".join(i)))
+        output.write("%s\n" % ("\t".join(i)))
+
 
